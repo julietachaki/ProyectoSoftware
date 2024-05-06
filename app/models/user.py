86fdd2143e98bf3fbb5c5a59d58ee8faa08e875a
julietachaki <chakijulieta@gmail.com>
2024-05-06 11:17:06 -0300

    #13

--- app/models/user.py
+++ app/models/user.py
@@ -1,40 +1,63 @@
+# Importa el decorador dataclass desde el módulo dataclasses
 from dataclasses import dataclass
 from typing import List
 
+# Importa la instancia db desde el módulo app, que parece ser un objeto de SQLAlchemy
 from app import db
 
+# Importa la clase UserData desde el archivo user_data.py en el mismo directorio
 from .user_data import UserData
 
 
+# Define una clase llamada User utilizando el decorador dataclass
 @dataclass(init=False, repr=True, eq=True)
-class User(db.Model):
-    __tablename__ = 'users'
-    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username: str = db.Column(db.String(80), unique=True, nullable=False)
-    password: str = db.Column(db.String(120), nullable=False)
-    email: str = db.Column(db.String(120), unique=True, nullable=False) 
-    data = db.relationship('UserData', uselist=False, back_populates='user') # type: ignore
+class User(
+    db.Model
+):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
+    __tablename__ = "users"  # Nombre de la tabla en la base de datos
+    id: int = db.Column(
+        db.Integer, primary_key=True, autoincrement=True
+    )  # Columna de clave primaria
+    username: str = db.Column(
+        db.String(80), unique=True, nullable=False
+    )  # Columna para el nombre de usuario
+    password: str = db.Column(
+        db.String(120), nullable=False
+    )  # Columna para la contraseña del usuario
+    email: str = db.Column(
+        db.String(120), unique=True, nullable=False
+    )  # Columna para el correo electrónico del usuario
 
+    # Relación con la tabla 'UserData' (datos de usuario), establecida a través de la propiedad 'user' en la clase UserData
+    data = db.relationship("UserData", uselist=False, back_populates="user")  # type: ignore
+
+    # Constructor de la clase User, que puede recibir un objeto UserData opcionalmente
     def __init__(self, user_data: UserData = None):
         self.data = user_data
 
-    def save(self) -> 'User':
+    """
+    Aplico el patrón Active Record https://www.martinfowler.com/eaaCatalog/activeRecord.html, donde el modelo se encarga de la persistencia de los datos.
+    Este patrón es muy útil para aplicaciones pequeñas y medianas, pero no es recomendable para aplicaciones grandes.
+    Puede llegar a contradecir los principios SOLID http://butunclebob.com/ArticleS.UncleBob.PrinciplesOfOod, ya que el modelo tiene responsabilidades de persistencia y de negocio.
+    """
+
+    def save(self) -> "User":
         db.session.add(self)
         db.session.commit()
         return self
-    
+
     def delete(self) -> None:
         db.session.delete(self)
         db.session.commit()
-    
+
     @classmethod
-    def all(cls) -> List['User']:
+    def all(cls) -> List["User"]:
         return cls.query.all()
 
     @classmethod
-    def find(cls, id: int) -> 'User':
+    def find(cls, id: int) -> "User":
         return cls.query.get(id)
 
     @classmethod
-    def find_by(cls, **kwargs) -> List['User']:
+    def find_by(cls, **kwargs) -> List["User"]:
         return cls.query.filter_by(**kwargs).all()
