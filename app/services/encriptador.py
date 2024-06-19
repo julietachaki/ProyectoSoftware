1366aaabc0e5931b8db76539451645958920a5b4
julietachaki <chakijulieta@gmail.com>
2024-06-19 18:05:53 -0300

    update encriptador

--- app/services/encriptador.py
+++ app/services/encriptador.py
@@ -5,21 +5,18 @@ from cryptography.fernet import Fernet
 
 
 class EncriptadorSV:
-    @staticmethod
-    def generate_key(key):
+    def generate_key(self,key):
         new_key = hashlib.sha256(key.encode()).digest()
         # Encode the key in base64 to make it valid for Fernet
         return base64.urlsafe_b64encode(new_key)
     
-    @staticmethod
-    def encrypt_content(content, key):
+    def encrypt_content(self,content, key):
         f = Fernet(key)
         encrypted_content = f.encrypt(content.encode('utf-8'))
         content = encrypted_content.decode('utf-8')
         return content
-    
-    @staticmethod
-    def decrypt_content(content, key):
+
+    def decrypt_content(self,content, key):
         f = Fernet(key)
         decrypted_content = f.decrypt(content.encode('utf-8'))
         return decrypted_content.decode('utf-8')
